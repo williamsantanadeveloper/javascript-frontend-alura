2e473fe3557d8398aeefc54edb890838583142e7
William Santana <williamandrade1058@gmail.com>
2024-01-25 12:06:49 -0300

    up

---
+++ alura-midi/exercises/ex01/main.js
@@ -0,0 +1,9 @@
+const keyOne = document.querySelector('.um');
+const keyTwo = document.querySelector('.dois');
+const keyThree = document.querySelector('.tres');
+const inputTel = document.querySelector('h1');
+
+
+inputTel.addEventListener('click', () => {
+    inputTel.style.display = 'none';
+});
---
+++ alura-midi/js/main.js
@@ -0,0 +1,34 @@
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador]
+
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
+}
+
+
+// let contador = 0;
+// while (contador < listaDeTeclas.length) {
+
+//     const tecla = listaDeTeclas[contador]
+
+//     const instrumento = tecla.classList[1];
+
+//     const idAudio = `#som_${instrumento}`;
+
+//     tecla.onclick = function () {
+//         tocaSom(idAudio);
+//     };
+//     contador++;
+// }
