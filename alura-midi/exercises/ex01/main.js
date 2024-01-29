c4c8d88f6062b886d060c16c96be3d9a07170500
William Santana <williamandrade1058@gmail.com>
2024-01-28 22:25:08 -0300

    feat: keyboard events

--- alura-midi/exercises/ex01/main.js
+++
@@ -1,9 +0,0 @@
-const keyOne = document.querySelector('.um');
-const keyTwo = document.querySelector('.dois');
-const keyThree = document.querySelector('.tres');
-const inputTel = document.querySelector('h1');
-
-
-inputTel.addEventListener('click', () => {
-    inputTel.style.display = 'none';
-});
--- alura-midi/js/main.js
+++ alura-midi/js/main.js
@@ -1,12 +1,19 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('Elemento ou seletor não encontrado!');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-    const tecla = listaDeTeclas[contador]
+    const tecla = listaDeTeclas[contador];
 
     const instrumento = tecla.classList[1];
 
@@ -15,6 +22,16 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     tecla.onclick = function () {
         tocaSom(idAudio);
     };
+
+    tecla.onkeydown = (evento) => {
+
+        if (evento.code === "Space" || evento.code === "Enter") {
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = (evento) => {
+        tecla.classList.remove('ativa');
+    }
 }
 
 
